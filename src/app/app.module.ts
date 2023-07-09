dc64c7f28594edc3299e9f194d7a25187f596752
milton munoz <livemilton@hotmail.com>
2023-07-08 22:32:40 -0500

    fin seccion 4 - Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//modulos
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent,
-    DashboardComponent,
-    BreadcrumbComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    BreadcrumbComponent,
+    HeaderComponent,
+    SidebarComponent,
+  ],
+  exports:[
+    BreadcrumbComponent,
+    HeaderComponent,
+    SidebarComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
